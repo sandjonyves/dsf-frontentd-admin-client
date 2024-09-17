d2c22bef4c3146f69e048bd4b21361196bcf5ff6
sandjonyves <sandjonyves@gmail.com>
2024-09-17 12:25:28 +0000

    deployent

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
 //export const BASE_URL = 'https://ztbtsgvqkn.us-east-2.awsapprunner.com/api/v1/';
-export const BASE_URL = 'http://localhost:8000/api/v1/';
+export const BASE_URL = 'https://dsf-backend.onrender.com'
+// 'http://localhost:8000/api/v1/';
